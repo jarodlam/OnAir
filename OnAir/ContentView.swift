fb65495d93e5629e4307897720161dbd02c60025
Jarod Lam <jarlam@deloitte.com.au>
2023-02-14 16:41:55 +1100

    Initial Commit

---
+++ OnAir/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OnAir
+//
+//  Created by Lam, Jarod on 14/2/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnAir/OnAirApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnAirApp.swift
+//  OnAir
+//
+//  Created by Lam, Jarod on 14/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct OnAirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
